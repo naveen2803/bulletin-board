b0b64186a44b375add909841ada16a172ceb9a0a
naveenm <naveen.malhotra@intelligentpathways.com.au>
2017-04-04 13:48:52 +1000

    initial commit

---
+++ src/Board.js
@@ -0,0 +1,76 @@
+import React from 'react'
+import './App.css'
+import Note from './Note'
+
+var Board = React.createClass({
+            propTypes: {
+                count: function(props, propName) {
+                    if(typeof props[propName] !== "number") {
+                        return new Error("the count must be a number")
+                    }
+
+                    if(props[propName] > 100) {
+                        return new Error('Creating ' + props[propName] + ' notes is ridiculous')
+                    }
+                }
+            },
+            getInitialState() {
+                return {
+                    notes: []
+                }
+            },
+            componentWillMount() {
+                if (this.props.count) {
+                    for(var i = 1; i <= this.props.count; i++)
+                    {
+                        this.add("Note " + i);
+                    }
+                }
+            },
+            nextId() {
+                this.uniqueId = this.uniqueId || 0
+                return this.uniqueId++
+            },
+            add(text) {
+                var n = this.state.notes;
+                n.push(
+                    {
+                        id: this.nextId(),
+                        note: text
+                    }
+                );
+
+                this.setState({notes: n})
+            },
+            update(newText, id) {
+                var notes = this.state.notes.map(
+                    note => (note.id !== id) ?
+                       note :
+                        {
+                            ...note,
+                            note: newText
+                        }
+                    )
+                this.setState({notes})
+            },
+            remove(id) {
+                var notes = this.state.notes.filter(note => note.id !== id)
+                this.setState({notes})
+            },
+            eachNote(note) {
+                return (<Note key={note.id}
+                              id={note.id}
+                              onChange={this.update}
+                              onRemove={this.remove}>
+                          {note.note}
+                        </Note>)
+            },
+            render() {
+                return (<div className='board'>
+                           {this.state.notes.map(this.eachNote)}
+                           <button onClick={() => this.add('New Note')}>+</button>
+                        </div>)
+            }
+        })
+
+export default Board
